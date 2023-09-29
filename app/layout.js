835deab714ac5020b40c19ee5188732c6447fb44
CameronJWalker <cameron.jr.walker@icloud.com>
2023-09-29 11:00:05 -0700

    installed clerk for auth and user management

--- app/layout.js
+++ app/layout.js
@@ -1,6 +1,7 @@
 import './globals.css'
 import { Inter } from 'next/font/google'
 import Head from 'next/head';
+import { ClerkProvider } from '@clerk/nextjs'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -11,11 +12,13 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en">
-       <Head>
-        <link rel="icon" type="image/svg+xml" href="/favicon.ico" />
-      </Head>
-      <body className={inter.className}>{children}</body>
-    </html>
+    <ClerkProvider>
+      <html lang="en">
+        <Head>
+          <link rel="icon" type="image/svg+xml" href="/favicon.ico" />
+        </Head>
+        <body className={inter.className}>{children}</body>
+      </html>
+    </ClerkProvider>
   )
 }
--- app/page.js
+++ app/page.js
@@ -1,7 +1,9 @@
+import { UserButton } from "@clerk/nextjs";
+
 export default function Home() {
   return (
     <main>
-      <h1> Hello </h1>
+      <UserButton afterSignOutUrl="/"/>
     </main>
   )
 }
