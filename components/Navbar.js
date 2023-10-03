caacc01238d813a9f7c8e58ba4ddc0d04d5b7108
CameronJWalker <cameron.jr.walker@icloud.com>
2023-10-02 18:57:14 -0700

    added navbar links with images that conditionally render based on route

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,23 +1,49 @@
+"use client"
+
 import Image from "next/image"
 import Link from "next/link"
+import { useRouter, usePathname } from 'next/navigation';
 
 function Navbar() {
-    return (
-        <nav class="h-20 mx-auto flex justify-center space-x-40 bg-honey ">
-            <Link href="/">
-                <Image class="my-1"src="/assets/Bee1.png" height={90} width={90}/>
+    const router = useRouter();
+    const pathname = usePathname()
+
+
+    console.log(pathname);
+        return (
+        <nav className="h-20 mx-auto flex justify-center space-x-40 bg-honey ">
+            <Link href="/books">
+                {
+                    pathname === "/books" 
+                    ? <Image className="my-1"src="/assets/Bee2.png" height={90} width={90} alt="bee"/>
+                    : <Image className="my-1"src="/assets/Bee1.png" height={90} width={80} alt="bee"/>
+                }
             </Link>
-            <Link href="/">
-                <Image class="my-1"src="/assets/Bee1.png" height={90} width={90}/>
+            <Link href="/#">
+                {
+                    pathname === "/#" 
+                    ? <Image className="my-1"src="/assets/Bee2.png" height={90} width={90} alt="bee"/>
+                    : <Image className="my-1"src="/assets/Bee1.png" height={90} width={80} alt="bee"/>
+                }
             </Link>
-            <div class=" z-5">
-                <Image src="/assets/BookHive.png" alt="logo" width={140} height={140}/>
+            <div className=" z-5">
+            <Link href="/">
+                <Image src="/assets/BookHive.png" alt="logo" width={140} height={140} />
+            </Link>
             </div>
-            <Link href="/">
-                <Image class="my-1"src="/assets/Bee1.2.png" height={90} width={90}/>
+            <Link href="/#">
+                {
+                    pathname === "/#" 
+                    ? <Image className="my-1"src="/assets/Bee2.1.png" height={90} width={90} alt="bee"/>
+                    : <Image className="my-1"src="/assets/Bee1.2.png" height={90} width={80} alt="bee"/>
+                }
             </Link>
-            <Link href="/">
-                <Image class="my-1"src="/assets/Bee1.2.png" height={90} width={90}/>
+            <Link href="/#">
+                {
+                    pathname === "/#" 
+                    ? <Image className="my-1"src="/assets/Bee2.1.png" height={90} width={90} alt="bee"/>
+                    : <Image className="my-1"src="/assets/Bee1.2.png" height={90} width={80} alt="bee"/>
+                }
             </Link>
         </nav>
     )
